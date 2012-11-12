c0d1c685f9b1d54e8262f0c9d4e05df804a194b3
Avnit Bambah <abambah@gmail.com>
2012-11-12 01:25:32 -0600

    New Project

---
+++ Groovy-test/src/people.groovy
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author avnit
+ *
+ */
+class people {
+
+	/**
+	 * 
+	 */
+	public people() {
+		// TODO Auto-generated constructor stub
+		def firstname 
+		def lastname 
+		def address
+		def zipcode
+		
+	}
+
+	static main(args) {
+		println "Call for people"
+	
+	}
+
+}
